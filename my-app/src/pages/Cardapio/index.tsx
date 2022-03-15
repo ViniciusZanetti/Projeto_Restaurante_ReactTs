a208477476e6647a6603024d549447e7d01878c0
ViniciusZanetti <58536337+ViniciusZanetti@users.noreply.github.com>
2022-03-15 12:13:40 -0300

    começo do projeto

---
+++ my-app/src/pages/Cardapio/index.tsx
@@ -0,0 +1,12 @@
+import styles from './Cardapio.module.scss'
+import Logo from '../../assets/logo.svg'
+
+export default function Cardapio() {
+  return (
+    <main>
+      <nav className={styles.menu}>
+          <img src={Logo} alt='logo do aluroni'/>
+      </nav>
+    </main>
+  );
+}
